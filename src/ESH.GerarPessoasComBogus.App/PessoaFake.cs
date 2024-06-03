85acc050de11f503aa3dc0da9b0884452804dfc9
Edmilson Hora <edmilson_hora@yahoo.com.br>
2024-06-03 07:54:46 -0300

    Inicial

---
+++ src/ESH.GerarPessoasComBogus.App/PessoaFake.cs
@@ -0,0 +1,36 @@
+﻿using Bogus;
+using Bogus.Extensions.Brazil;
+using Bogus.Extensions.Denmark;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESH.GerarPessoasComBogus.App
+{
+    public static class PessoaFake
+    {
+        public static List<PessoaJson> Listar(int quantidade)
+        {
+            string [] sexo = { "Masculino", "Feminino" };
+
+            var pessoaFake = new Faker<PessoaJson>("pt_BR")
+                .RuleFor(c => c.Nome, f => f.Person.FullName)
+                .RuleFor(c => c.Sexo, f => f.PickRandom(sexo))
+                .RuleFor(c => c.Data_nasc, f => f.Person.DateOfBirth.ToString())
+                .RuleFor(c => c.Email, f => f.Person.Email)
+                .RuleFor(c => c.Telefone_fixo, f => f.Person.Phone.ToString())
+                .RuleFor(c => c.Endereco, f => f.Person.Address.Street)
+                .RuleFor(c => c.CEP, f => f.Address.ZipCode())
+                .RuleFor(c => c.Celular, f => f.Person.Phone.ToString())
+                .RuleFor(c => c.Bairro, f => f.Address.County())
+                .RuleFor(c => c.Cidade, f => f.Address.City())
+                .RuleFor(c => c.CPF, f => f.Person.Cpf())
+                .RuleFor(c => c.RG, f => f.Person.Cpr());
+            
+
+              return pessoaFake.Generate(quantidade);            
+        }
+    }
+}
---
+++ src/ESH.GerarPessoasComBogus.App/PessoaJson.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESH.GerarPessoasComBogus.App
+{
+    public class PessoaJson
+    {        
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string RG { get; set; }
+        public string Data_nasc { get; set; }
+        public string Sexo { get; set; }
+        public string Email { get; set; }
+        public string CEP { get; set; }
+        public string Endereco { get; set; }
+        public int Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Telefone_fixo { get; set; }
+        public string Celular { get; set; }
+    }
+}
